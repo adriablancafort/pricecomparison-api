0953ca85986c865e5f0fbedd72e763286075bc85
Adrià <adriablancafort@gmail.com>
2025-05-23 00:08:48 +0200

    prices endpoint

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/prices")
+def get_prices():
+    prices = [
+        {
+            "price": "$19.99",
+            "link": "https://www.amazon.com/example-product",
+            "store": "Amazon"
+        },
+        {
+            "price": "$24.99",
+            "link": "https://www.mediamarkt.es/es/product/_example-product-1234567.html",
+            "store": "MediaMarkt"
+        },
+        {
+            "price": "$17.99",
+            "link": "https://www.pccomponentes.com/example-product",
+            "store": "PCComponentes"
+        }
+    ]
+    
+    return prices
